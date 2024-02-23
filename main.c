5d49969e26a29b8c7c5e256a38323fb62e1d0127
z3a1 <paul.colonia@outlook.com>
2024-02-23 00:49:09 -0800

    Implemented Thread Separation

--- main.c
+++ main.c
@@ -4,68 +4,135 @@
 #include <string.h>
 #include <unistd.h>
 #include <pthread.h>
+#include "List.h"
 
 #define MSG_MAX_LEN 1024
+
+/**
+ * Send thread should lock its current mutex then send a signal for the recieve condition variable
+ * Then after we unlock the current thread again
+ * Brackets within will be our CS?
+ * Vice versa for the recieve
+*/
 
 struct threadData{
   int socket;
   struct addrinfo *servinfo;
+  List** givenList;
+};
+
+struct ListIo{
+  List** givenList;
 };
 
 void *sendThread(void* data)
 {
-  struct threadData* sendData = (struct threadData*) data;
+    struct ListIo* sendData = (struct ListIo*) data;
 
-  while(1){
-  char message[MSG_MAX_LEN];
-  // SEND MESSAGE
- 
-  fgets(message, sizeof(message), stdin);
+    while(1){
+      char message[MSG_MAX_LEN];
+      // SEND MESSAGE
+    
+      fgets(message, sizeof(message), stdin);
+      if(List_append(&sendData->givenList,message) == -1){
+        printf("Error adding message to outbound list\n");
+      }
+      else{
+        printf("Successfully added item to outbound list\n");
+      }
 
-  int send_len = sendto(sendData->socket, message, strlen(message), 0, sendData->servinfo->ai_addr, sendData->servinfo->ai_addrlen);
-  if (send_len == -1)
-  {
-    perror("sendto error");
   }
-  else
-  {
-    printf("Sent %d bytes\n\n", send_len);
-  }
-
-}
-  return NULL;
+    return NULL;
 }
 
 void *receiveThread(void* data)
 {
-  struct threadData* receiveData = (struct threadData*) data;
+  struct ListIo* receiveData = (struct ListIo*) data;
 
   // Receive data
  struct sockaddr_storage servAddr;
     while (1)
   {
-  
-  char serverBuffer[MSG_MAX_LEN];
-  socklen_t servAddrLen = sizeof servAddr;
+    char serverBuffer[MSG_MAX_LEN];
+    socklen_t servAddrLen = sizeof servAddr;
 
-  int recv_len = recvfrom(receiveData->socket, serverBuffer, sizeof(serverBuffer), 0, (struct sockaddr *)&servAddr, &servAddrLen);
-  if (recv_len == -1)
-  {
-    perror("recvfrom error");
-  }
+    int recv_len = recvfrom(receiveData->socket, serverBuffer, sizeof(serverBuffer), 0, (struct sockaddr *)&servAddr, &servAddrLen);
+    if (recv_len == -1)
+    {
+      perror("recvfrom error");
+    }
 
-  serverBuffer[recv_len] = '\0'; // Null-terminate the received data
+    serverBuffer[recv_len] = '\0'; // Null-terminate the received data
 
-  printf("Received message: %s\n", serverBuffer); // Receive data
+    // printf("Received message: %s\n", serverBuffer); // Receive data
 
+    int addStatus = List_append(&recieveData->givenList,serverBuffer);
+    if(addStatus == -1){
+      printf("Message has not been recieved!\n");
+    }
+    else{
+      printf("Message recieved successfully\n");
+    }
 
   }
 
   return NULL;
 }
 
+void* sendToClient(void* data){
+  struct threaddata* sendData = (struct threadData*) data;
+
+  while(1){
+    char message[MSG_MAX_LEN];
+
+    void* messageToSend = List_trim(&sendData->givenList);
+
+    if(messageToSend != NULL){
+      message = (char*) messageToSend;
+      int send_len = sendto(sendData->socket, message, strlen(message), 0, sendData->servinfo->ai_addr, sendData->servinfo->ai_addrlen);
+      if (send_len == -1)
+      {
+        perror("sendto error");
+      }
+      else
+      {
+        printf("Sent %d bytes\n\n", send_len);
+      }
+    }
+    else{
+      printf("No messages to send\n");
+    }
+  }
+  
+}
+
+void* displayMessage(void* data){
+  //TODO
+  struct ListIo* inBoundList = (struct ListIo*) data;
+
+  while(1){
+    char serverBuffer[MSG_MAX_LEN];
+    void* messageToDisplay = List_trim(&sendData->givenList);
+    if(messageToDisplay != NULL){
+      serverBuffer = (char*) messageToDisplay;
+      if (send_len == -1)
+      {
+        perror("sendto error");
+      }
+      else
+      {
+        printf("Message: %s\n\n", )serverBuffer;
+      }
+    }
+    else{
+      printf("No messages recieved\n");
+    }
+  }
+}
+
 int main(int argc, char *argv[])
 {
+
   /////////////////////////
   // Server Socket
   ////////////////////////
@@ -146,20 +213,35 @@ int main(int argc, char *argv[])
   ////////////////////////
   pthread_t receiveThreadPID;
   pthread_t sendThreadPID;
-  struct threadData threadSendData;
-  struct threadData threadReceiveData;
+  struct ListIo threadSendData;
+  struct ListIo threadReceiveData;
 
-  threadSendData.socket = clientSocket;
-  threadSendData.servinfo = servinfo2;
+  /*********List and respective threads************/
 
-  threadReceiveData.socket = serverSocket;
-  threadReceiveData.servinfo = servinfo;
-   printf("\nStart Messaging:\n" );
+  pthread_t inboundThread;
+  pthread_cond_t okToRecvCondVar = PTHREAD_COND_INITIALIZER;
+  pthread_mutex_t okToRecvMutex = PTHREAD_MUTEX_INITIALIZER;
+
+  pthread_t outboundThread;
+  pthread_cond_t okToDisplayCondVar = PTHREAD_COND_INITIALIZER;
+  pthread_mutex_t okToDisplayMutex = PTHREAD_MUTEX_INITIALIZER;
+
+  List* outBoundList = List_create();
+  List* inBoundList = List_create();
+
+  /************************************************/
+  threadSendData.givenList = &outBoundList;
+
+  threadReceiveData.givenList = &inBoundList;
+
+  printf("\nStart Messaging:\n" );
   pthread_create(&sendThreadPID, NULL, sendThread, (void*)&threadSendData);
   pthread_create(&receiveThreadPID, NULL, receiveThread, (void*)&threadReceiveData);
   
   pthread_join(sendThreadPID, NULL);
   pthread_join(receiveThreadPID, NULL);
+  pthread_join(inboundThread,NULL);
+  pthread_join(outboundThread,NULL);
  
 
   close(clientSocket);
